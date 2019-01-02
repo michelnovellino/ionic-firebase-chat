e371b8ee0c4e5041aa89a7c8769951d94ba2606b
michelnovellino <michelnovellino.programador@gmail.com>
2019-01-02 12:20:44 -0400

    lo que falto del domingo

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import  {AngularFireDatabase,AngularFireObject}  from 'angularfire2/database';
 import { Profile } from '../../models/profile/profile.model';
 import { User } from 'firebase';
+import { take } from 'rxjs/operators';
 
 @Injectable()
 export class DataProvider {
@@ -9,6 +10,14 @@ export class DataProvider {
   profileObject: AngularFireObject<Profile>
 
   constructor(private database: AngularFireDatabase) {
+  }
+
+
+
+
+  getProfile(user:User){
+    this.profileObject = this.database.object(`/profiles/${user.uid}`)
+    return this.profileObject.pipe(take(1))
   }
 
   async saveProfile(user: User ,profile: Profile){
